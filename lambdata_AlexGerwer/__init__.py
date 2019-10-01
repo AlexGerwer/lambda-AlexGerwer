bb9a0b7a83fd933a347a94e8e06ed3a80c564794
Alex Gerwer <asg_akn@yahoo.com>
2019-10-01 00:34:56 -0700

    Added some proof of concept utility function

---
+++ lambdata_AlexGerwer/__init__.py
@@ -0,0 +1,10 @@
+"""
+lambdata is a collection of data science helper functions
+"""
+import pandas as pd
+import numpy as np
+
+# this is sample code
+# declare global variables
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(5))
---
+++ lambdata_AlexGerwer/df_utils.py
@@ -0,0 +1,6 @@
+"""
+Utility function for working with DataFrame
+"""
+import pandas as pd
+
+TEST = pd.DataFrame([1, 2, 3])
